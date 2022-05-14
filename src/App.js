198846edc1f9b03c964629add6d40084ebde9a5b
olivia kasten <odkasten@gmail.com>
2022-05-14 11:14:25 -0400

    set font styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        Hello hello
         <Button>Welcome to Cakebook.</Button>
       </header>
     </div>
