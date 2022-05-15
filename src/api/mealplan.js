072c17034282f43b0d7d09934117f60b1a4e4142
andreawatkins <awatki22@depaul.edu>
2022-05-15 18:58:19 -0500

    add mealplan base pages

---
+++ src/api/mealplan.js
@@ -0,0 +1,24 @@
+const API_URL = 'somestring';
+
+export function getMealplans() {
+  return [
+    {
+      id: 1,
+      week: 'May 15-May 21',
+    },
+    {
+      id: 2,
+      week: 'May 22-May 28'
+    },
+    {
+      id: 3,
+      name: 'May 29 - June 4'
+    }
+  ]
+}
+
+export function createNewMealplan(week) {
+  //placeholder
+  //fetch(API_URL).then(r => r.json())
+  return true;
+}
---
+++ src/mealplan/Mealplan.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Mealplan = () => {
+  return (
+    <div>
+        <div>
+            Sunday Mealplan
+        </div>
+        <div>
+            Monday Mealplan
+        </div>
+        <div>
+            Tuesday Mealplan
+        </div>
+        <div>
+            Thursday Mealplan
+        </div>
+        <div>
+            Friday Mealplan
+        </div>
+        <div>
+            Saturday Mealplan
+        </div>
+    </div>
+  )
+}
+
+export default Mealplan
