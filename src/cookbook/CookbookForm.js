b3528fa8b1107b296e7cbde91a28f817cd35f53e
olivia kasten <odkasten@gmail.com>
2022-05-14 21:22:47 -0400

    basic form, needs some more work

--- src/cookbook/CookbookForm.js
+++ src/cookbook/CookbookForm.js
@@ -1,7 +1,15 @@
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 function CookbookForm() {
   return(
     <div className="centerContainer cookbookForm">
-      Hello
+      <h4 className="title">Create a New Cookbook</h4>
+      <p className="title">What do you want to call it?</p>
+      <Form className="formInput">
+        <Form.Control type="name" placeholder="Enter cookbook's name"/>
+        <Button variant="primary" type="submit">Save</Button>
+      </Form>
     </div>
   )
 }
