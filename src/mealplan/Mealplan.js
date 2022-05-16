ce2cde41080cd067311490b6efb4e544acf40094
andreawatkins <awatki22@depaul.edu>
2022-05-16 14:15:36 -0500

    create new cookbook functionality

--- src/mealplan/Mealplan.js
+++ src/mealplan/Mealplan.js
@@ -1,26 +1,95 @@
 import React from 'react'
+import { getMealplans } from '../api/mealplan'
 
 const Mealplan = () => {
+    getMealplans();
   return (
     <div>
-        <div>
-            Sunday Mealplan
+        <div style = {{backgroundColor: '#FEFEFE', marginTop: '20px',marginRight: '500px'}}>
+            <h2>Sunday Mealplan:</h2>
+            <div>Breakfast Recipe: 
+
+            </div>
+               <div>
+                    Lunch Recipe: 
+                </div>
+                <div>
+                Dinner Recipe: 
+            </div>
         </div>
-        <div>
-            Monday Mealplan
+        <div style = {{backgroundColor: '#FEFEFE', marginTop: '20px',marginRight: '500px'}}>
+        <h2>Monday Mealplan:</h2>
+        <div>Breakfast Recipe: 
+
+            </div>
+               <div>
+                    Lunch Recipe: 
+                </div>
+                <div>
+                Dinner Recipe: 
+            </div>
         </div>
-        <div>
-            Tuesday Mealplan
+        <div style = {{backgroundColor: '#FEFEFE', marginTop: '20px',marginRight: '500px'}}>
+            <h2>Tuesday Mealplan:</h2>
+            <div>Breakfast Recipe: 
+
+            </div>
+               <div>
+                    Lunch Recipe: 
+                </div>
+                <div>
+                Dinner Recipe: 
+            </div>
         </div>
-        <div>
-            Thursday Mealplan
+        <div style = {{backgroundColor: '#FEFEFE', marginTop: '20px',marginRight: '500px'}}>
+        <h2>Wednesday Mealplan:</h2>
+        <div>Breakfast Recipe: 
+
+            </div>
+               <div>
+                    Lunch Recipe: 
+                </div>
+                <div>
+                Dinner Recipe: 
+            </div>
         </div>
-        <div>
-            Friday Mealplan
+        <div style = {{backgroundColor: '#FEFEFE', marginTop: '20px',marginRight: '500px'}}>
+            <h2>Thursday Mealplan:</h2>
+            <div>Breakfast Recipe: 
+
+            </div>
+               <div>
+                    Lunch Recipe: 
+                </div>
+                <div>
+                Dinner Recipe: 
+            </div>
         </div>
-        <div>
-            Saturday Mealplan
+        <div style = {{backgroundColor: '#FEFEFE', marginTop: '20px',marginRight: '500px'}}>
+            <h2>Friday Mealplan:</h2>
+            <div>Breakfast Recipe: 
+
+            </div>
+               <div>
+                    Lunch Recipe: 
+                </div>
+                <div>
+                Dinner Recipe: 
+            </div>
         </div>
+        <div style = {{backgroundColor: '#FEFEFE', marginTop: '20px',marginRight: '500px'}}>
+            <h2>Saturday Mealplan:</h2>
+            <div>Breakfast Recipe: 
+
+            </div>
+               <div>
+                    Lunch Recipe: 
+                </div>
+                <div>
+                Dinner Recipe: 
+            </div>
+        </div>
+       
     </div>
   )
 }
